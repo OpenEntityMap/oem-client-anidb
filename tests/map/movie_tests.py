72f9f249c82123d7e6a8d777e526cdc78050f074
Dean Gardiner <me@dgardiner.net>
2016-07-24 23:50:52 +1200

    Added tests for TMDb movie mappings

--- tests/map/movie_tests.py
+++ tests/map/movie_tests.py
@@ -10,6 +10,13 @@ import pytest
 #
 
 
+def test_matches_anidb_movie(client):
+    assert client['tmdb:movie'].to('anidb').map('823') == MovieMatch({'anidb': '83'})
+    assert client['tmdb:movie'].to('anidb').map('16990') == MovieMatch({'anidb': '321'})
+    assert client['tmdb:movie'].to('anidb').map('18837') == MovieMatch({'anidb': '3392'})
+    assert client['tmdb:movie'].to('anidb').map('25446') == MovieMatch({'anidb': '7183'})
+
+
 def test_matches_imdb_episode(client):
     assert client['anidb'].to('imdb').map('1045', EpisodeIdentifier(1, 1)) == MovieMatch({'imdb': 'tt1125254'})
 
@@ -17,6 +24,14 @@ def test_matches_imdb_episode(client):
 def test_matches_imdb_movie(client):
     assert client['anidb'].to('imdb').map('1043', MovieIdentifier()) == MovieMatch({'imdb': 'tt0142235'})
     assert client['anidb'].to('imdb').map('1045') == MovieMatch({'imdb': 'tt1125254'})
+
+
+def test_matches_tmdb_movie(client):
+    assert client['anidb'].to('tmdb:movie').map('83') == MovieMatch({'tmdb:movie': '823'})
+    assert client['anidb'].to('tmdb:movie').map('321') == MovieMatch({'tmdb:movie': '16990'})
+    assert client['anidb'].to('tmdb:movie').map('399') == MovieMatch({'tmdb:movie': '77192'})
+    assert client['anidb'].to('tmdb:movie').map('440') == MovieMatch({'tmdb:movie': '41421'})
+
 
 
 #
